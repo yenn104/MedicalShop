f76999773851b6070fdb4b2578831efc9e621c16
Bao Yen <nneyel104@gmail.com>
2022-12-09 01:34:39 +0700

    login

--- MedicalShop/Controllers/HomeController.cs
+++ MedicalShop/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using MedicalShop.Models;
 using MedicalShop.Models.Entities;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace MedicalShop.Controllers
@@ -51,51 +54,72 @@ namespace MedicalShop.Controllers
     public ActionResult Login(TaiKhoan account, string returnUrl)
     {
       MedicalShopContext context = new MedicalShopContext();
-      //if (ModelState.IsValid)
-      //{
-      string user = account.UserName;
-      string pass = account.Password;
-      var acc = context.TaiKhoan.FirstOrDefault(s => s.UserName.Equals(user) && s.Password.Equals(pass));
-      if (acc != null)
+      if (ModelState.IsValid)
       {
-        if (acc.Roles == true)
+        string user = account.UserName;
+        string pass = account.Password;
+        var acc = context.TaiKhoan.FirstOrDefault(s => s.UserName.Equals(user) && s.Password.Equals(pass));
+        if (acc != null)
         {
-          //FormsAuthentication.SetAuthCookie(acc.UserName, false);
-          if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
-              && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+          if (acc.Roles == true)
           {
-            return Redirect(returnUrl);
+            //FormsAuthentication.SetAuthCookie(acc.UserName, false);
+            if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                              && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+            {
+              return Redirect(returnUrl);
+            }
+            else
+            {
+              NhanVien nv = context.NhanVien.FirstOrDefault(n => n.Id.Equals(acc.Idnv));
+              var claims = new List<Claim>
+              {
+                  new Claim(ClaimTypes.Name, account.UserName),
+                  new Claim(ClaimTypes.Role, account.Password),
+                  new Claim("NV", nv.TenNv),
+              };
+              var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+              HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+              return RedirectToAction("Index", "Home");
+            }
           }
           else
           {
-            //FormsAuthentication.SetAuthCookie(account.UserName, true);
-            //Session["TaiKhoan"] = acc;
-            return RedirectToAction("Index", "Home");
-          }
-        }
-        else
-        {
-          if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
-              && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
-          {
-            return Redirect(returnUrl);
-          }
+            if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                              && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+            {
+              return Redirect(returnUrl);
+            }
+            else
+            {
+              KhachHang kh = context.KhachHang.FirstOrDefault(s => s.Id.Equals(acc.Idkh));
+              var claims = new List<Claim>
+              {
+                  new Claim(ClaimTypes.Name, account.UserName),
+                  new Claim(ClaimTypes.Role, account.Roles.ToString()),
+                  new Claim("KH", kh.TenKh),
+              };
+              var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+              HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+              return RedirectToAction("News", "Home");
+            }
+           }
+         }
           else
           {
-            //FormsAuthentication.SetAuthCookie(account.Username, true);
-            //Session["TaiKhoan"] = acc;
-            //return RedirectToAction("Index", "Home");
+            ModelState.AddModelError("", "Tài khoản hoặc mật khẩu không chính xác!");
           }
-          //return RedirectToAction("Index", "Home");
         }
+        return View();
       }
-      //else
-      //{
-      //  ModelState.AddModelError("", "Tài khoản hoặc mật khẩu không chính xác!");
-      //}
-      //}
-      return View();
+
+    public ActionResult Logout()
+    {
+      HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+      return RedirectToAction("Index");
     }
+
+
 
     //public IActionResult Privacy()
     //{
@@ -103,9 +127,9 @@ namespace MedicalShop.Controllers
     //}
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      public IActionResult Error()
+      {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      }
     }
   }
-}
