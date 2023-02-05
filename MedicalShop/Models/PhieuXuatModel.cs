db0dc45198e43f2b55e7b3c6e32a105ed3a1c42d
Bao Yen <nneyel104@gmail.com>
2023-02-05 10:26:46 +0700

    px

---
+++ MedicalShop/Models/PhieuXuatModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalShop.Models
+{
+  public class PhieuXuatModel
+  {
+    //public PhieuNhap phieuNhap { get; set; }
+    public string NgayHd { get; set; }
+    public string SoHd { get; set; }
+    public int? NCC { get; set; }
+    public string Note { get; set; }
+
+    public List<ChiTietPhieuXuatTam> listOfCTPXT { get; set; }
+  }
+}
