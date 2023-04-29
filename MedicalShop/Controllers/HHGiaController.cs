c835c334028f901c2e298e96aabc88761c964772
Bao Yen <nneyel104@gmail.com>
2023-04-29 19:58:50 +0700

    ok1xiu

--- MedicalShop/Controllers/HHGiaController.cs
+++ MedicalShop/Controllers/HHGiaController.cs
@@ -1,4 +1,5 @@
-﻿using MedicalShop.Models.Entities;
+﻿using MedicalShop.Models;
+using MedicalShop.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -42,6 +43,59 @@ namespace MedicalShop.Controllers
       ViewBag.ListCTNK = listctnk;
       ViewBag.IDHH = idhh;
       return PartialView();
+    }
+
+
+
+    [HttpPost("/updatePrices")]
+    public JsonResult updateRoles([FromBody] IEnumerable<PriceModel> list)
+    {
+      MedicalShopContext context = new MedicalShopContext();
+      int idUser = int.Parse(User.Claims.ElementAt(2).Type);
+      int idcn = int.Parse(User.Claims.ElementAt(4).Value);
+      foreach (var item in list)
+      {
+        HhGia gia = context.HhGia.FirstOrDefault(n => n.Idhh == item.Idhh);
+        if (gia == null)
+        {
+          HhGia giaa = new HhGia();
+          giaa.Idhh = item.Idhh;
+          if (item.TiLe == 0)
+          {
+            giaa.Price = item.Price;
+          }
+          if(item.Price == 0)
+          {
+            giaa.TiLe = item.TiLe;
+          }
+          giaa.CreatedBy = idUser;
+          giaa.CreatedDate = DateTime.Now;
+          giaa.ModifiedBy = idUser;
+          giaa.ModifiedDate = DateTime.Now;
+          giaa.Idcn = idcn;
+          giaa.Active = true;
+          context.HhGia.Add(giaa);
+        }
+        else
+        {
+          if (item.TiLe != 0)
+          {
+            gia.TiLe = item.TiLe;
+            gia.Price = null;
+          }
+          if (item.Price != 0)
+          {
+            gia.Price = item.Price;
+            gia.TiLe = null;
+          }
+          gia.ModifiedBy = idUser;
+          gia.ModifiedDate = DateTime.Now;
+          context.HhGia.Update(gia);
+        }
+      }
+      context.SaveChanges();
+      return Json(data: "Cập nhật thành công!");
+
     }
 
 
---
+++ MedicalShop/Models/PriceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalShop.Models
+{
+  public class PriceModel
+  {
+    public int? Idhh { get; set; }
+    public double? TiLe { get; set; }
+    public double? Price { get; set; }
+  }
+}
